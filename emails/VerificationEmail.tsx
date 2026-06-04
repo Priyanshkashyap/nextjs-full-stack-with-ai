41d3971a08ee040d36bcb6210198f15a25d7c373
Priyanshkashyap <kpriyansh2121@gmail.com>
2026-06-04 12:04:11 +0530

    email verification component

---
+++ emails/VerificationEmail.tsx
@@ -0,0 +1,82 @@
+import {
+  Html,
+  Head,
+  Font,
+  Preview,
+  Heading,
+  Row,
+  Section,
+  Text,
+  Button,
+} from "react-email"; // looks a bit dfferent from normal html tags but converted to those only underneath
+
+interface VerificationEmailProps {
+  username: string;
+  otp: string;
+}
+
+export default function VerificationEmail({
+  username,
+  otp,
+}: VerificationEmailProps) {
+  return (
+    <Html lang="en" dir="ltr">
+      <Head>
+        <title>Verification Code</title>
+
+        <Font
+          fontFamily="Roboto"
+          fallbackFontFamily="Verdana"
+          webFont={{
+            url: "https://fonts.gstatic.com/s/roboto/v27/KFOmCnqEu92Fr1Mu4mxKKTU1Kg.woff2",
+            format: "woff2",
+          }}
+          fontWeight={400}
+          fontStyle="normal"
+        />
+      </Head>
+
+      <Preview>Here&apos;s your verification code: {otp}</Preview> 
+      // not shown in frontend
+
+      <Section>
+        <Row>
+          <Heading as="h2">Hello {username},</Heading>
+        </Row>
+
+        <Row>
+          <Text>
+            Thank you for registering. Please use the following verification
+            code to complete your registration:
+          </Text>
+        </Row>
+
+        <Row>
+          <Text>{otp}</Text>
+        </Row>
+
+        <Row>
+          <Text>
+            If you did not request this code, please ignore this email.
+          </Text>
+        </Row>
+
+        <Row>
+  <Button
+    href={`http://localhost:3000/verify/${username}`}
+    style={{
+      backgroundColor: "#000",
+      color: "#fff",
+      padding: "12px 20px",
+      borderRadius: "5px",
+      textDecoration: "none",
+      display: "inline-block",
+    }}
+  >
+    Verify Here
+  </Button>
+</Row>
+      </Section>
+    </Html>
+  );
+}
